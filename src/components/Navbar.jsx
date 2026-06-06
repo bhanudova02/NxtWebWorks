82e018f867344ee5bd9904ecca7675d8070d6f14
bhanudova02 <bhanudova02@gmail.com>
2026-06-06 19:30:57 +0530

    style: reduce border opacity in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   const closeMenu = () => setIsOpen(false);
 
   return (
-    <nav className="fixed left-0 right-0 top-0 z-50 w-full border-b-2 border-slate-200 bg-white">
+    <nav className="fixed left-0 right-0 top-0 z-50 w-full border-b-2 border-slate-200/60 bg-white">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-15 items-center justify-between md:h-16">
           <Link to="/" onClick={closeMenu} className="flex items-center">
